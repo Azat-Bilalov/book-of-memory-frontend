5c36b9ba60ec9a73b9e5d0ec7c588df111bb8e74
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2023-11-13 01:13:24 +0300

    🚀 Деплой

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/book-of-memory/",
   css: {
     modules: {
       localsConvention: "camelCase",
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
   resolve: {
